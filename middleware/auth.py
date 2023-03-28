6e8abb28a89ff30ef15fcaf037191a8912d8cedc
Khom Outsourcify <khom@outsourcify.net>
2023-03-28 12:40:19 +0700

    [pref] split api v2 for compression

--- middleware/auth.py
+++ middleware/auth.py
@@ -6,11 +6,12 @@ from runtime_config import api_key
 
 
 # Decorator that verifies the API key
-def verify_api_key(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        request_api_key = request.headers.get("API-Key")
-        if request_api_key is None or request_api_key != api_key:
-            return jsonify({"error": "Invalid API key"}), 401
-        return func(*args, **kwargs)
-    return wrapper
+def verify_api_key(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if request.headers.get('API-Key') == api_key:
+            return f(*args, **kwargs)
+        else:
+            return jsonify({'status': 'error', 'message': 'Invalid API key'}), 401
+
+    return decorated_function
--- resources/pdf.py
+++ resources/pdf.py
@@ -138,6 +138,67 @@ def convert_to_pdf():
             optimize_size=('fonts', 'images'),
         )
 
+        # Create a response with the compressed PDF data
+        response = make_response(pdf)
+        response.headers['Content-Type'] = 'application/pdf'
+        response.headers['Content-Disposition'] = 'attachment; filename="{}.pdf"'.format(
+            filename)
+
+        logger.info('Converted HTML to PDF from IP: {}'.format(
+            request.remote_addr))
+
+        return response
+
+    except Exception as e:
+        # add ip address to log
+        logger.error('Error converting HTML to PDF from IP: {}, Reason {}'.format(
+            request.remote_addr, str(e)))
+        return jsonify({'error': 'Error converting HTML to PDF: {}'.format(str(e))}), 500
+
+
+@pdf_api.route('/api/v2/convert-to-pdf', methods=['POST'])
+@verify_api_key
+def convert_to_pdf_v2():
+    """
+    Converts HTML to PDF and returns the compressed PDF as a response
+
+    Note:
+        Use @page css to set the page size and orientation
+
+    Request headers:
+        API-key (str): The API key to use for authentication
+
+    Request data:
+        html (str): The HTML data to be converted
+        filename (str, optional): The filename to use for the PDF (default: "converted")
+
+    Returns:
+        A response with the compressed PDF data and the appropriate Content-Type and Content-Disposition headers
+    """
+    if not request.is_json:
+        return jsonify({'error': 'No JSON data provided'}), 400
+
+    # Get the HTML data and filename from the request
+    data = request.get_json(silent=True)
+    html = data.get('html')
+    filename = data.get('filename', 'converted')
+
+    if not html:
+        return jsonify({'error': 'No HTML data provided'}), 400
+
+    # Validate the filename
+    if not validate_filename(filename):
+        return jsonify({'error': 'Invalid filename'}), 400
+
+    # Sanitize the HTML to prevent XSS attacks
+    html = sanitize_html(html)
+
+    try:
+        # Convert the HTML to PDF
+        pdf = weasyprint.HTML(string=html).write_pdf(
+            optimize_size=('fonts', 'images'),
+        )
+
         # Compress the PDF
         pdf = compress_pdf(pdf, power=4)
 
